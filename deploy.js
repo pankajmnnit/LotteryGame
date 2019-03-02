e4bdbe5603f946985f7d7d7b3b0124aaaaa84252
pankajmnnit <pankaj.joshimnnit@gmail.com>
2019-03-02 14:33:23 +0530

    added gui support

--- deploy.js
+++ deploy.js
@@ -1,13 +1,13 @@
 const HDWalletProvider=require('truffle-hdwallet-provider');
 const Web3=require('web3');
 const {interface,bytecode}=require('./compile');
-console.log("hello");
+
 const provider=new HDWalletProvider(
 	 'disorder siren pink couch position wealth current disagree dress deny net lucky',
 	 'https://rinkeby.infura.io/e74be1eb922c45c1adf37684bc961620'
 	 );
 const web3 = new Web3(provider);
-console.log("hello");
+
 //asysnc can be used inside function
 const deploy=async()=>{
 	const accounts=await web3.eth.getAccounts();
@@ -17,6 +17,7 @@ const deploy=async()=>{
   const result= await new web3.eth.Contract(JSON.parse(interface)).
    deploy({data:bytecode}).
    send({gas:'1000000',from:accounts[0]});
+   console.log(interface);
 
    console.log('Contract deployed to',result.options.address);
 };
